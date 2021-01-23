0b348fa361d4e1a10edec8aaeb5f825b6f8099bd
Antonio Dell'Ava <antonio.dellava@gmail.com>
2021-01-23 18:06:33 +0100

    first case added

--- src/toCamelcase.test.ts
+++ src/toCamelcase.test.ts
@@ -1,5 +1,16 @@
 import toCamelcase from "./toCamelcase";
 
-test('dummy', () => {
-    expect(toCamelcase("mondo")).toBe("ciao! mondo!!!!");
+interface Case {
+    input: string,
+    output: string,
+}
+
+const cases:Case[] = [
+    {input: "il sol che move il sole", output: "ilSolCheMoveIlSole"}
+];
+
+test('it should transform a string in camel case', () => {
+    for(const aCase of cases) {
+        expect(toCamelcase(aCase.input)).toBe(aCase.output);
+    }
 });
--- src/toCamelcase.ts
+++ src/toCamelcase.ts
@@ -1,3 +1,18 @@
 export default function (word: string): string {
-    return `ciao! ${word}!!!!`;
+
+    const wordArray:string[] = word.split(" ");
+
+    let result = "";
+
+    for(let i = 0, len = wordArray.length; i < len; i ++){
+        const word = wordArray[i];
+        let tempString = word.toLowerCase();
+        if(i){
+            tempString = tempString.substr(0, 1).toUpperCase() + tempString.substr(1);
+        }
+
+        result += tempString;
+    }
+
+    return result;
 };
